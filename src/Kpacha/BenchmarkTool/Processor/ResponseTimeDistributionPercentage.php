932ddaf76b6a20c750fae6be1f58deded2caf40a
Kpacha <kpacha666@gmail.com>
2014-01-05 15:25:59 +0100

    fixing the loader discovering strategy

---
+++ src/Kpacha/BenchmarkTool/Processor/ResponseTimeDistributionPercentage.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Kpacha\BenchmarkTool\Processor;
+
+/**
+ * Description of ResponseTimeDistribution
+ *
+ * @author Kpacha <kpacha666@gmail.com>
+ */
+class ResponseTimeDistribution extends ResponseTime
+{
+
+    protected function getCommandOptions($name, $input, $output)
+    {
+        return <<<EOD
+-e "set terminal pngcairo transparent enhanced font \"arial,10\" fontscale 1.0 size 500, 350; \
+    set size 1,1; set grid y; set key left top; \
+    set xlabel 'request'; set ylabel 'ms'; \
+    set autoscale fix; \
+    set datafile separator '\t'; \
+    set title \"Response time distribution\"; \
+    set output '{$this->outputPath}$output.sequence.png'; \
+    stats '$input' using 5 prefix 'A' nooutput; \
+    plot \"$input\" using 5 with lines title 'Response', A_mean title 'Mean', A_median title 'Median';"
+EOD;
+    }
+
+}
+
